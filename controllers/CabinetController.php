4e2ec90947c9e4564cfb60449918fdb04698d70e
Max <shvarcko@ya.ru>
2016-09-21 18:55:28 +0300

    base(start success)

---
+++ controllers/CabinetController.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Контроллер CabinetController
+ * Кабинет пользователя
+ */
+class CabinetController
+{
+
+    /**
+     * Action для страницы "Кабинет пользователя"
+     */
+    public function actionIndex()
+    {
+        // Получаем идентификатор пользователя из сессии
+        $userId = User::checkLogged();
+
+        // Получаем информацию о пользователе из БД
+        $user = User::getUserById($userId);
+
+        // Подключаем вид
+        require_once(ROOT . '/views/cabinet/index.php');
+        return true;
+    }
+
+    /**
+     * Action для страницы "Редактирование данных пользователя"
+     */
+    public function actionEdit()
+    {
+        // Получаем идентификатор пользователя из сессии
+        $userId = User::checkLogged();
+
+        // Получаем информацию о пользователе из БД
+        $user = User::getUserById($userId);
+
+        // Флаг результата
+        $result = false;
+
+        // Обработка формы
+        if (isset($_POST['submit'])) {
+            // Если форма отправлена
+            // Получаем данные из формы редактирования
+            $name = $_POST['name'];
+            $password = $_POST['password'];
+
+            // Флаг ошибок
+            $errors = false;
+
+            // Валидируем значения
+
+            if ($errors == false) {
+                // Если ошибок нет, сохраняет изменения профиля
+                $result = User::edit($userId, $name, $password);
+            }
+        }
+
+        // Подключаем вид
+        require_once(ROOT . '/views/cabinet/edit.php');
+        return true;
+    }
+
+}
